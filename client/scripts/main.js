bcb11057b4f16577f6884398bd13016efa44a601
TheoLeanse <theoleanse@gmail.com>
2016-03-18 19:04:36 +0000

    add transitions

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,6 +2,7 @@
 // var rows = $('.sortable tbody tr');
 
 $('.search').keyup(function () {
+  closeAllSubTables();
   var _this = this;
   // Show only matching TR, hide rest of them
   $.each($('.sortable tbody tr'), function () {
@@ -12,19 +13,21 @@ $('.search').keyup(function () {
   });
 });
 
-d3.csv('./sample.csv', data => {
 
-  const nestedData = d3.nest()
+d3.csv('./sample.csv', data => {
+  const nestedByFund = d3.nest()
           .key(d => d.fund)
           .entries(data);
 
-  const fundData = extractFundData(nestedData);
-  const fundColumns = Object.keys(fundData[0]);
-  const subTables = formatSubTables(nestedData);
+  const fundData 		= extractFundData(nestedByFund);
+  const fundColumns 	= Object.keys(fundData[0]);
+  const subTableData 	= formatSubTables(nestedByFund);
+  const fundTable 		= createFundTable(fundColumns, fundData);
+  const tableWrapper 	= document.querySelector('.table-container');
 
-  const fundTable = createTable(fundColumns, fundData);
-  const tableWrapper = document.querySelector('.table-container');
   tableWrapper.appendChild(fundTable); // do we want to append like this, or use d3 (can we use d3 for the subTables?)
+
+  new Tablesort(document.querySelector('.sortable'));
 
   // FORMAT DATA
 
@@ -36,7 +39,6 @@ d3.csv('./sample.csv', data => {
         'Portfolio value': fund.values[0]['portfolio value']
       };
     });
-
   }
 
   function formatSubTables(data) {
@@ -58,7 +60,7 @@ d3.csv('./sample.csv', data => {
 
   // CREATE A TABLE FROM DATA
 
-  function createTable(columns, rows) {
+  function createFundTable(columns, rows) {
     var table = d3.select(document.createElement('table'));
     var thead = table.append('thead');
     var tbody = table.append('tbody');
@@ -68,16 +70,17 @@ d3.csv('./sample.csv', data => {
       .data(columns)
       .enter()
       .append('th')
-      .attr('class', 'sortable sort-up')
+      //.attr('class', 'sortable')
+      .attr('class', d => d === 'Fund' ? 'sort-up' : null)
       .text(column => column);
 
-    var rows = tbody.selectAll('tr')
+    var tableRows = tbody.selectAll('tr')
           .data(rows)
           .enter()
           .append('tr')
           .attr('data-fund', d => d.fund); // this is specific
 
-    var cells = rows.selectAll('td')
+    var cells = tableRows.selectAll('td')
           .data(row => {
             return columns.map(column => {
               return {
@@ -88,13 +91,61 @@ d3.csv('./sample.csv', data => {
           })
           .enter()
           .append('td')
-          .text(d => d.value);
+          .text(d => d.value)
+          .attr('class', 'clickable')
+          .attr('class', d => {
+            console.log(d.value);
+            if (d.column === 'Weight in portfolio' || d.column === 'Portfolio value') {
+              console.log("MOOP");
+              return 'clickable right-aligned-cell';
+            }
+          });
 
     table[0][0].classList.add('sortable', 'o-table', 'o-table--compact');
     return table[0][0]; // because the actual element is inside a d3 structure
   }
 
+  function createSubTable(columns, rows) {
+    var table = d3.select(document.createElement('table'));
+    var thead = table.append('thead');
+    var tbody = table.append('tbody');
+
+    thead.append('tr')
+      .selectAll('th')
+      .data(columns)
+      .enter()
+      .append('th')
+      .text(column => column)
+      .attr('class', 'notclickable');
+
+    var tableRows = tbody.selectAll('tr')
+          .data(rows)
+          .enter()
+          .append('tr');
+
+    var cells = tableRows.selectAll('td')
+          .data(row => {
+            return columns.map(column => {
+              return {
+                column: column,
+                value: row[column]
+              };
+            });
+          })
+          .enter()
+          .append('td')
+          .text(d => d.value)
+
+    table[0][0].classList.add('o-table', 'o-table--compact');
+    return table[0][0]; // because the actual element is inside a d3 structure
+  }
+
   // DISPLAY DATA ON USER EVENTS
+
+  document.querySelector('thead').addEventListener('click', (e) => {
+    e.stopPropagation(); // but then sometimes won't reorder / show correct order?
+    closeAllSubTables();
+  });
 
   const rowElements = Array.from(document.querySelectorAll('tbody tr')); // use dom-delegate instead
   rowElements.forEach(row => {
@@ -102,15 +153,17 @@ d3.csv('./sample.csv', data => {
   });
 
   function rowClickHandler(e) {
-    e.stopImmediatePropagation();
+    e.stopPropagation();
+
     const row = e.target.parentElement; // because it's going to be a <td>
     if (row.classList.contains('opened')) {
       closeSlideout(row);
     } else {
+      closeAllSubTables();
       const clickedFund = row.dataset.fund;
-      const subData = subTables.filter(fund => fund.Fund === clickedFund);
+      const subData = subTableData.filter(fund => fund.Fund === clickedFund);
       const subColumns = Object.keys(subData[0].bonds[0]);
-      const subTable = createTable(subColumns, subData[0].bonds);
+      const subTable = createSubTable(subColumns, subData[0].bonds);
       openSlideout(row, subTable);
     }
   }
@@ -119,15 +172,25 @@ d3.csv('./sample.csv', data => {
     row.classList.add('opened');
 
     const newRow = row.parentElement.insertRow(row.rowIndex);
+    newRow.classList.add('subtable');
+
     const fullCell = newRow.insertCell();
     fullCell.colSpan = 9;
+    fullCell.style.paddingLeft = '100px'; // percentage
+
     const slider = document.createElement('div');
     slider.classList.add('sliding');
+
+
     fullCell.appendChild(slider);
+    slider.appendChild(subTable);
     window.getComputedStyle(slider).height;
-    //slider.style.height = '100px';
-    slider.appendChild(subTable); // again, not using d3 to append
-    slider.style.height = subTable.clientHeight;
+    console.log('height: ', subTable.offsetHeight);
+    slider.style.height = `${subTable.clientHeight}px`;
+
+    subTable.style.visibility = 'visible';
+
+
   }
 
   function closeSlideout(row) {
@@ -136,20 +199,23 @@ d3.csv('./sample.csv', data => {
     const slider = document.querySelector('.sliding');
 
     slider.addEventListener('transitionend', (e) => {
-      e.stopImmediatePropagation();
-      slider.style.display = 'none';
       row.parentElement.deleteRow(row.rowIndex);
-    }, false); // do we need stopImmediateProp... and useCapture: false?
+    });
 
     // start the transition
     slider.style.height = '0px';
-    row.parentElement.deleteRow(row.rowIndex);
 
   }
 
-  function closeAllSubTables() {
-    // TODO: when something clicked, close all open drawer elements
-  };
+});
 
-  new Tablesort(document.querySelector('.sortable'));
-})
+function closeAllSubTables() {
+  const openedRows = Array.from(document.querySelectorAll('.opened'));
+  openedRows.forEach(row => row.classList.remove('opened'));
+
+  const subtables = Array.from(document.querySelectorAll('.subtable'));
+  subtables.forEach(subtable => {
+    const tbody = document.querySelector('tbody');
+    tbody.removeChild(subtable);
+  });
+}
